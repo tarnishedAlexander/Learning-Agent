c716a76265d24bac66ea007dd828e16fa2aa3826
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-08-16 17:15:45 -0400

    feat[api-client]: adapt create class API

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 const API_URL = import.meta.env.API_URL;
 
 export const apiClient = axios.create({
-  baseURL: API_URL,
-  withCredentials: true, // para las cookies
+  baseURL: API_URL || "http://localhost:3000/",
   headers: {
     "Content-Type": "application/json",
   },
