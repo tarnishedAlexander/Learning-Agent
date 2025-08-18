29089762ac463177b204590bae268c2bbb58e46e
Nigthshitf <burgos3du@gmail.com>
2025-08-17 22:15:17 -0400

    fix/endpointPOST&GET, upload&download

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,18 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './core/prisma/prisma.module';
 import { RbacModule } from './modules/rbac/rbac.module';
 import { IdentityModule } from './modules/identity/identity.module';
+import { ConfigModule } from '@nestjs/config'
+import { DocumentModule } from './modules/repository/document.module';
 
-@Module({ imports: [PrismaModule, RbacModule, IdentityModule] })
+@Module({
+  imports: [
+    PrismaModule,
+    RbacModule,
+    IdentityModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DocumentModule
+  ],
+  controllers: [],
+  providers: [],
+})
 export class AppModule {}
