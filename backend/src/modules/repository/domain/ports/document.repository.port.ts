3ad57f28668c463197bd373f1960c0ca09613f3b
Ivan Ticona <ivan.ticona.v@gmail.com>
2025-08-20 19:25:03 -0400

    module renamed

--- backend/src/modules/repository/domain/ports/document.repository.port.ts
+++
@@ -1,7 +0,0 @@
-import { Readable } from 'stream';
-import { DocumentEntity } from '../entities/document.entity';
-
-export interface DocumentRepository {
-  save(document: Omit<DocumentEntity, 'id'>): Promise<DocumentEntity>;
-  download(minIOKey: string): Promise<Readable>
-}
