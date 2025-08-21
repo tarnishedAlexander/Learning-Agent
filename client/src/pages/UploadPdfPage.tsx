e9ecb4aaf0512c246884ff4935055e95a636a682
Diego_Orellana <diegoorellanaggch13@gmail.com>
2025-08-21 10:17:49 -0400

    feat:Crear componente de carga de archivos

--- client/src/pages/UploadPdfPage.tsx
+++ client/src/pages/UploadPdfPage.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Card } from "antd";
-import PdfUploader from "../components/PdfUploader";
+import Uploader from "../components/Uploader";
 
 const UploadPdfPage: React.FC = () => {
-  const handleComplete = (file: File, response?: unknown) => {
-    console.log("Archivo finalizado:", file);
-    console.log("Respuesta del servidor (si hubiera):", response);
-  };
-
   return (
     <div style={{ padding: 20 }}>
-      <Card title="Subir PDF" style={{ maxWidth: 900 }}>
-        <PdfUploader onUploadComplete={handleComplete} />
+      <Card title="Subir PDF" style={{ maxWidth: 1100, margin: "0 auto" }}>
+        <Uploader />
       </Card>
     </div>
   );
