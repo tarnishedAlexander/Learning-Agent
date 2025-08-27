ecb32435aacab56874def8afc279fbe4023872d7
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-08-26 22:40:41 -0400

    feat[US-001]: update prisma migrations

---
+++ backend/prisma/migrations/20250827022310_academic_courses/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `teacherId` on the `Classes` table. All the data in the column will be lost.
+  - Added the required column `courseId` to the `Classes` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Classes" DROP CONSTRAINT "Classes_teacherId_fkey";
+
+-- DropIndex
+DROP INDEX "public"."RolePermission_permissionId_idx";
+
+-- AlterTable
+ALTER TABLE "public"."Classes" DROP COLUMN "teacherId",
+ADD COLUMN     "courseId" TEXT NOT NULL,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "public"."Course" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "teacherId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Course" ADD CONSTRAINT "Course_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "public"."TeacherProfile"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Classes" ADD CONSTRAINT "Classes_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "public"."Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
