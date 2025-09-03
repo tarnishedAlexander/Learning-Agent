ee15402a0ea323ae9fcf3947fc0e6b892f8a453e
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-09-03 14:40:08 -0400

    feat[US005]: Use Logger on academic_management module

--- backend/src/modules/academic_management/application/commands/create-course.usecase.ts
+++ backend/src/modules/academic_management/application/commands/create-course.usecase.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { COURSE_REPO, TEACHER_REPO } from "../../tokens";
 import type { CourseRepositoryPort } from "../../domain/ports/courses.repository.ports";
 import type { ProfessorRepositoryPort } from "../../domain/ports/teacher.repository.ports";
@@ -6,6 +6,7 @@ import { NotFoundError } from "src/shared/handler/errors";
 
 @Injectable()
 export class CreateCourseUseCase {
+    private readonly logger = new Logger(CreateCourseUseCase.name)
     constructor (
         @Inject(COURSE_REPO) private readonly courseRepo: CourseRepositoryPort,
         @Inject(TEACHER_REPO) private readonly teacherRepo: ProfessorRepositoryPort
@@ -14,7 +15,7 @@ export class CreateCourseUseCase {
     async execute (input: { teacherId: string; name: string }) {
         const teacher = await this.teacherRepo.findByUserId(input.teacherId);
         if (!teacher) {
-            console.error(`No Teacher with ID ${input.teacherId}`)
+            this.logger.error(`No Teacher with ID ${input.teacherId}`)
             throw new NotFoundError(`No se ha podido recuperar la información del Docente`)
         }
 
--- backend/src/modules/academic_management/application/queries/get-classes-by-student.usecase.ts
+++ backend/src/modules/academic_management/application/queries/get-classes-by-student.usecase.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { ENROLLMENT_REPO, CLASSES_REPO, STUDENT_REPO } from "../../tokens";
 import type { EnrollmentRepositoryPort } from "../../domain/ports/enrollment.repository.ports";
 import type { ClassesRepositoryPort } from "../../domain/ports/classes.repository.ports";
@@ -8,6 +8,7 @@ import { NotFoundError } from "src/shared/handler/errors";
 
 @Injectable()
 export class GetClassesByStudentUseCase {
+    private readonly logger = new Logger(GetClassesByStudentUseCase.name)
     constructor(
         @Inject(ENROLLMENT_REPO) private readonly enrollmentRepo: EnrollmentRepositoryPort,
         @Inject(CLASSES_REPO) private readonly classesRepo: ClassesRepositoryPort,
@@ -17,7 +18,7 @@ export class GetClassesByStudentUseCase {
     async execute(studentId: string): Promise<Classes[]> {
         const student = await this.studentRepo.findByUserId(studentId);
         if (!student) {
-            console.error(`Student not found with id ${studentId}`)
+            this.logger.error(`Student not found with id ${studentId}`)
             throw new NotFoundError(`No se ha podido recuperar la información del estudiante`)
         }
         
