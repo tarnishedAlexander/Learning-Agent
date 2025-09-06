a5792d52eca5be70eba556532cddf1bef97d4e5c
Paul Landaeta <paulwilkerlf@gmail.com>
2025-09-05 21:14:26 -0400

    fix: store user info to avoid missed the info after refreshing

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import apiClient from "../api/apiClient";
 
 export type User = {
@@ -15,17 +16,25 @@ type UserState = {
   fetchUser: () => Promise<void>;
 };
 
-export const useUserStore = create<UserState>((set, _get) => ({
-  user: null,
-  setUser: (u) => set({ user: u }),
-  fetchUser: async () => {
-    try {
-      const res = await apiClient.get("/auth/me");
-      debugger;
-      set({ user: res.data });
-    } catch (e) {
-      console.error("Error fetching user (store):", e);
-      set({ user: null });
+export const useUserStore = create<UserState>()(
+  persist(
+    (set, _get) => ({
+      user: null,
+      setUser: (u) => set({ user: u }),
+      fetchUser: async () => {
+        try {
+          const res = await apiClient.get("/auth/me");
+          set({ user: res.data });
+        } catch (e) {
+          console.error("Error fetching user (store):", e);
+          set({ user: null });
+        }
+      },
+    }),
+    {
+      name: "user",
+      partialize: (state) => ({ user: state.user }),
+      version: 1,
     }
-  },
-}));
+  )
+);
