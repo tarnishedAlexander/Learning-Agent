aa1ff25a331fbf5ac1e4a82fd24f3617b371d0a3
Paul Landaeta <paulwilkerlf@gmail.com>
2025-09-11 20:41:57 -0400

    fix: fixing header in course responsive

--- client/src/pages/academic_management/CourseDetailPage.tsx
+++ client/src/pages/academic_management/CourseDetailPage.tsx
@@ -472,13 +472,7 @@ export function CourseDetailPage() {
               key="general"
             >
               <div style={{ padding: "32px" }}>
-                <div
-                  style={{
-                    display: "grid",
-                    gridTemplateColumns: "1fr 1fr",
-                    gap: "24px",
-                  }}
-                >
+                <div className="grid grid-cols-1 min-[1000px]:grid-cols-2 gap-6">
                   <div>
                     <Text strong style={{ fontSize: "14px" }}>
                       Nombre del curso:
